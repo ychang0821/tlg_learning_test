a7851ddf9dbd089b647a49bc4991c0f608a13081
ychang0821 <68558597+ychang0821@users.noreply.github.com>
2020-07-22 13:06:22 -0700

    Add files via upload
    add day_three.py

---
+++ day_three.py
@@ -0,0 +1,46 @@
+# 1) Create two variables a, b that's set equal to some numbers. Print the following:
+a = 10
+b = 3
+# a) a + b
+print(a + b)
+# b) a * b
+print(a * b)
+# c) a - b
+print(a - b)
+# d) a / b
+print(a / b)
+# e) a % b
+print(a % b)
+
+# 2) Create a string x and set it equal to 'python'. Do the following on the string:
+x = 'python'
+# a) print the element at the 0th index
+print(x[0])
+# b) print the element at the last index
+print(x[-1])
+print(x[len(x)-1])
+print(x[5])
+# c) print the size of the string
+print(len(x))
+# d) uppercase the letters in the string by using the upper() method
+print(x.upper())
+
+# 3) Create a list called nums that by default is empty. Do the following:
+nums = []
+# a) Add 10
+nums.append(10)
+# b) Add 100
+nums.append(100)
+# c) Add 10
+nums.append(10)
+# d) Add 500
+nums.append(500)
+# e) Add 56
+nums.append(56)
+# f) Remove the last element
+nums.pop()
+print(nums)
+# g) Sort the list
+nums.sort()
+print(nums)
+
