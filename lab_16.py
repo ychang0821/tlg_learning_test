25de054215fcdea24b3d3b493f1b3894ce69ea0a
ychang0821 <68558597+ychang0821@users.noreply.github.com>
2020-07-23 10:37:30 -0700

    Add files via upload

---
+++ lab_16.py
@@ -0,0 +1,17 @@
+hostname = "MTG"
+if hostname == "MTG":
+    print("The hostname was found to be mtg")
+
+hostname = input("What value should we set for hostname?")
+if hostname == "MTG":
+    print("The hostname was found to be mtg")
+
+hostname = input("What value should we set for hostname?")
+if hostname.lower() == "mtg":
+    print("The hostname was found to be mtg")
+
+hostname = input("What value should we set for hostname?")
+if hostname.lower() == "mtg":
+    print("The hostname was found to be mtg")
+    print("hostname matches expected config")
+print("Exiting the script")
